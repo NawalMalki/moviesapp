1d23100a0d77f3218287ba2f04444fe16d89ddd7
NawalMalki <malkinawal2004@gmail.com>
2025-03-27 22:29:01 +0100

    Navbar responsive

--- src/app/navbar-diff-css/navbar-diff-css.component.ts
+++ src/app/navbar-diff-css/navbar-diff-css.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-diff-css',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-diff-css.component.css'
 })
 export class NavbarDiffCssComponent {
+  isMenuOpen = false;
 
+  toggleMenu(event: Event) {
+    event.stopPropagation(); // Empêche la propagation pour éviter la fermeture immédiate
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeMenu(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar-container')) {
+      this.isMenuOpen = false; // Ferme la navbar si on clique en dehors
+    }
+  }
 }
--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu(event: Event) {
+    event.stopPropagation(); // Empêche la propagation pour éviter la fermeture immédiate
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeMenu(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar-container')) {
+      this.isMenuOpen = false; // Ferme la navbar si on clique en dehors
+    }
+  }
 }
